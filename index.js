d045950801b7d1d3cdb2fcb8d89de5351119c72e
ange837 <ange83783@gmail.com>
2025-11-10 02:51:00 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+  
+    // Simulation : identifiants "admin" / "1234"
+    if (username === "admin" && password === "1234") {
+      window.location.href = "inter.html";
+    } else {
+      alert("Identifiants incorrects.");
+    }
+  });
+  
