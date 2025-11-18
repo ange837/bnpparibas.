ac80aa071bb3d129f6966c5ec2662a0e44b51f1d
ange837 <ange83783@gmail.com>
2025-11-18 18:37:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     const password = document.getElementById("password").value.trim();
   
     // Simulation : identifiants "admin" / "1234"
-    if (username === "admin" && password === "1234") {
+    if (username === "greenberg9" && password === "Ottawa22@") {
       window.location.href = "inter.html";
     } else {
       alert("Identifiants incorrects.");
     }
   });
+
   
